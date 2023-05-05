a8a61813541bb17261cfcaf8f6b08702e5e42c9c
gnehzza <alex.zhongkai.zheng@gmail.com>
2023-05-05 18:40:08 -0400

    added getgoogletags

--- services/google.js
+++ services/google.js
@@ -1,14 +1,28 @@
 const axios = require("axios");
 const mongo = require("./mongo");
+const vision = require("@google-cloud/vision");
+const { GoogleAuth } = require("google-auth-library");
 
-// TODO
 const getGoogleTags = async (url) => {
   console.log(`google.getGoogleTags(${url})`);
 
-  return axios.get().catch((error) => {
+  const auth = new GoogleAuth({
+    keyFilename: "services/service-account-key.json",
+    scopes: ["https://www.googleapis.com/auth/cloud-platform"],
+  });
+
+  const client = new vision.ImageAnnotatorClient({ auth });
+
+  try {
+    const [result] = await client.labelDetection(url);
+    const labels = result.labelAnnotations;
+    const tags = labels.map((label) => label.description);
+    console.log(tags);
+    return tags;
+  } catch (error) {
     console.log(`google.getGoogleTags(${url}) failed:\n${error}`);
     throw error;
-  });
+  }
 };
 
 const getImageTags = async (image) => {
