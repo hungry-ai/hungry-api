b8e9e149ca20b66f0cfbe147df4bc0ff09a3121c
Nozomi Yasuda <nozomi.yasuda.4649@gmail.com>
2023-06-12 12:00:15 -0400

    hungryai.js getStories() testing  (#5)
    * instagramwebhook test
    
    * syntax
    
    * fixed request
    
    * hungryai, getStories test
    
    * small syntax changes

--- tests/instagram.test.js
+++ tests/instagram.test.js
@@ -1,13 +1,6 @@
 const { InstagramWebhook } = require("../models/Webhook");
 const instagram = require("../services/instagram");
-{
-  /*}
-test("testing instagram.getStories", () =>
-  instagram
-    .getStories("balconycarspotting")
-    .then((stories) => expect(stories).toStrictEqual("ha")));
-*/
-}
+
 test("testing instagram.parseWebhook", () => {
   const testWebHook = new InstagramWebhook({
     object: "instagram",
@@ -49,7 +42,7 @@ test("testing instagram.parseWebhook", () => {
       url: "https://i5.walmartimages.com/asr/9f8b7456-81d0-4dc2-b422-97cf63077762.0ddba51bbf14a5029ce82f5fce878dee.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF",
     },
   ];
-  parsed.then((parsed) => {
-    expect(parsed).toStrictEqual(expectedJSON);
+  parsed.then((x) => {
+    expect(x).toStrictEqual(expectedJSON);
   });
 });
